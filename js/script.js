f3859a3f0202edb51ded12594af59ea9d4a387e1
Oleksandra Antonova <werba2010@gmail.com>
2023-08-08 18:16:37 -0400

    adding condition window.location.pathname

--- js/script.js
+++ js/script.js
@@ -1,109 +1,117 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Change background (.section2, index.html)
-  const section2 = document.querySelector('.section-2');
-  const headingOne = document.getElementById('headingOne');
-  const headingTwo = document.getElementById('headingTwo');
-  const headingThree = document.getElementById('headingThree');
+if (window.location.pathname === "../index.html") {
 
-  if (section2) {
-    headingOne.addEventListener('click', function () {
-      section2.style.background = 'url(../images/wine.jpg) center/cover no-repeat';
-    });
+  // Change background (.section2, index.html)
+  document.addEventListener('DOMContentLoaded', function () {
+    const section2 = document.querySelector('.section-2');
+    const headingOne = document.getElementById('headingOne');
+    const headingTwo = document.getElementById('headingTwo');
+    const headingThree = document.getElementById('headingThree');
 
-    headingTwo.addEventListener('click', function () {
-      section2.style.background = 'url(../images/jazz.jpeg) center/cover no-repeat';
-    });
+    if (section2) {
+      headingOne.addEventListener('click', function () {
+        section2.style.background = 'url(../images/wine.jpg) center/cover no-repeat';
+      });
 
-    headingThree.addEventListener('click', function () {
-      section2.style.background = 'url(../images/meal.jpg) center/cover no-repeat';
-    });
-  } 
+      headingTwo.addEventListener('click', function () {
+        section2.style.background = 'url(../images/jazz.jpeg) center/cover no-repeat';
+      });
 
-});
+      headingThree.addEventListener('click', function () {
+        section2.style.background = 'url(../images/meal.jpg) center/cover no-repeat';
+      });
+    }
 
-// Gallery (gallery.html)
-
-function toggleFullScreen() {
-  this.classList.toggle("full");
+  });
+  
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const galleryImages = document.querySelectorAll(".gallery-image");
-  for (let i of galleryImages) {
-    i.onclick = toggleFullScreen;
+if (window.location.pathname === "../gallery.html") {
+  // Gallery (gallery.html)
+
+  function toggleFullScreen() {
+    this.classList.toggle("full");
   }
-});
 
+  document.addEventListener('DOMContentLoaded', function () {
+    const galleryImages = document.querySelectorAll(".gallery-image");
+    for (let i of galleryImages) {
+      i.onclick = toggleFullScreen;
+    }
+  });
+  
+}
 
-// Form validation (reservations.html)
+if (window.location.pathname === "../reservations.html") {
 
-const userNameInput = document.getElementById("name")
-const userDateInput = document.getElementById("date")
-const userTimeInput = document.getElementById("time")
-const userGuestsInput = document.getElementById("guests")
-const submitButton = document.getElementById("submit")
-const resetButton = document.getElementById("reset")
-const reservationsResultElement = document.getElementById("reservationsresult")
+  // Form validation (reservations.html)
 
-submitButton.addEventListener('click', function () {
+  const userNameInput = document.getElementById("name")
+  const userDateInput = document.getElementById("date")
+  const userTimeInput = document.getElementById("time")
+  const userGuestsInput = document.getElementById("guests")
+  const submitButton = document.getElementById("submit")
+  const resetButton = document.getElementById("reset")
+  const reservationsResultElement = document.getElementById("reservationsresult")
 
-  reservationsname.textContent = ""
-  reservationsdate.textContent = ""
-  reservationstime.textContent = ""
-  reservationsguests.textContent = ""
-  reservationsresult.textContent = ""
-  reservationsResultElement.classList.remove("reservationsresult");
-  console.log("Button CLicked")
+  submitButton.addEventListener('click', function () {
 
-  let userName = userNameInput.value.trim()
-  let userDate = userDateInput.value
-  let userTime = userTimeInput.value
-  let userGuests = userGuestsInput.value
-  console.log(userName)
-  console.log(userDate)
-  console.log(userTime)
-   console.log(reservationsResultElement)
+    reservationsname.textContent = ""
+    reservationsdate.textContent = ""
+    reservationstime.textContent = ""
+    reservationsguests.textContent = ""
+    reservationsresult.textContent = ""
+    reservationsResultElement.classList.remove("reservationsresult");
+    console.log("Button CLicked")
 
-  switch (true) {
-    case userName === "":
-      reservationsname.textContent = "Please enter your name!"
-      break;
-    case userName.length <= 2:
-      reservationsname.textContent = "Please enter 3 or more characters!"
-      break;
-    case /\d/.test(userName):
-      reservationsname.textContent = "Name must contain letters only. Please check spelling!"
-      break;
-    case userDate === "":
-      reservationsdate.textContent = "Please select a date!"
-      break;
-    case userTime === "":
-      reservationstime.textContent = "Please select a time!"
-      break;
-    case userGuests === "":
-      reservationsguests.textContent = "Please select number of guests!"
-      break;
-    case userGuests == 0:
-      reservationsguests.textContent = "The number of guests must be greater than 0. Please check the entered value!"
-      break;
-    default:
-      switch (true) {
-        case userGuests == 1:
-          reservationsResultElement.classList.add("reservationsresult");
-          reservationsresult.textContent = `${userName}, thanks for your reservation!
+    let userName = userNameInput.value.trim()
+    let userDate = userDateInput.value
+    let userTime = userTimeInput.value
+    let userGuests = userGuestsInput.value
+    console.log(userName)
+    console.log(userDate)
+    console.log(userTime)
+    console.log(reservationsResultElement)
+
+    switch (true) {
+      case userName === "":
+        reservationsname.textContent = "Please enter your name!"
+        break;
+      case userName.length <= 2:
+        reservationsname.textContent = "Please enter 3 or more characters!"
+        break;
+      case /\d/.test(userName):
+        reservationsname.textContent = "Name must contain letters only. Please check spelling!"
+        break;
+      case userDate === "":
+        reservationsdate.textContent = "Please select a date!"
+        break;
+      case userTime === "":
+        reservationstime.textContent = "Please select a time!"
+        break;
+      case userGuests === "":
+        reservationsguests.textContent = "Please select number of guests!"
+        break;
+      case userGuests == 0:
+        reservationsguests.textContent = "The number of guests must be greater than 0. Please check the entered value!"
+        break;
+      default:
+        switch (true) {
+          case userGuests == 1:
+            reservationsResultElement.classList.add("reservationsresult");
+            reservationsresult.textContent = `${userName}, thanks for your reservation!
           We will be happy to meet you on ${userDate} at ${userTime}`
-          break;
-        case userGuests > 1:
-          reservationsResultElement.classList.add("reservationsresult");
-          reservationsresult.textContent = `${userName}, thanks for reservation!
+            break;
+          case userGuests > 1:
+            reservationsResultElement.classList.add("reservationsresult");
+            reservationsresult.textContent = `${userName}, thanks for reservation!
           We will be happy to meet your company of ${userGuests} people on ${userDate} at ${userTime}`
-          break;
-      }
-      break;
-  }
-});
+            break;
+        }
+        break;
+    }
+  });
 
-resetButton.addEventListener("dblclick", function () {
+  resetButton.addEventListener("click", function () {
     console.log("Button reset CLicked")
     reservationsname.textContent = ""
     reservationsdate.textContent = ""
@@ -111,8 +119,10 @@ resetButton.addEventListener("dblclick", function () {
     reservationsguests.textContent = ""
     reservationsresult.textContent = ""
     reservationsResultElement.classList.remove("reservationsresult");
-    userNameInput.value  = ""
-    userDateInput.value = "" 
+    userNameInput.value = ""
+    userDateInput.value = ""
     userTimeInput.value = ""
     userGuestsInput.value = ""
-});
+  });
+
+}
